b27bfe55b2bcf15e124aefb855dd373078c39f8d
Christian Fraß <frass@greenscale.de>
2016-11-14 10:54:04 +0100

    bugfix

--- source/tasks/task_lesscss.ts
+++ source/tasks/task_lesscss.ts
@@ -13,8 +13,8 @@ class class_task_lesscss extends class_task {
 			"sub": sub,
 			"active": active,
 			"parameters": {
-				"inputs": inputs = [],
-				"output": output = null,
+				"inputs": inputs_raw = [],
+				"output": output_raw = null,
 			}
 		} : {
 			name ?: string;
@@ -26,12 +26,18 @@ class class_task_lesscss extends class_task {
 			};
 		}
 	) {
-		let inputs_ : Array<lib_path.class_filepointer> = lib_call.use(
-			inputs,
+		if (inputs_raw == undefined) {
+			throw (new Error(class_task.errormessage_mandatoryparamater("lesscss", name, "inputs")));
+		}
+		let inputs : Array<lib_path.class_filepointer> = lib_call.use(
+			inputs_raw,
 			x => x.map(y => lib_path.filepointer_read(y))
 		);
-		let output_ : lib_path.class_filepointer = lib_call.use(
-			output,
+		if (output_raw == undefined) {
+			throw (new Error(class_task.errormessage_mandatoryparamater("lesscss", name, "output")));
+		}
+		let output : lib_path.class_filepointer = lib_call.use(
+			output_raw,
 			x => lib_path.filepointer_read(x)
 		);
 		let filepointer_temp : lib_path.class_filepointer = new lib_path.class_filepointer(
@@ -40,19 +46,19 @@ class class_task_lesscss extends class_task {
 		);
 		super(
 			name, sub, active,
-			inputs_,
-			[output_],
+			inputs,
+			[output],
 			[
 				new class_action_mkdir(
-					output_.location
+					output.location
 				),
 				new class_action_concat(
-					inputs_,
+					inputs,
 					filepointer_temp
 				),
 				new class_action_lessc(
 					filepointer_temp,
-					output_
+					output
 				),
 			]
 		);
