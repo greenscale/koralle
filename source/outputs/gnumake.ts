762d1c1b9043913ed0e207235e9838cc89b95c46
Christian Fraß <frass@greenscale.de>
2017-01-13 15:30:48 +0100

    fix

--- source/outputs/gnumake.ts
+++ source/outputs/gnumake.ts
@@ -207,7 +207,8 @@ class class_target_gnumake extends class_target_regular<string> {
 					[
 						// `--directory=${workdir}`,
 						// `--file=${filepointer.as_string(configuration.system)}`,
-						`-f ${filepointer.as_string(configuration.system)}`,
+						`-f`,
+						`${filepointer.as_string(configuration.system)}`,
 					],
 					{}
 				);
